d081bbca9184a781413130b374525f728843faa9
robroyx3m <33149720+robroyx3m@users.noreply.github.com>
2017-11-01 11:22:01 +0100

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let mut tries = 0; 
+
+    loop{
+
+        tries += 1;
+
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("This isn't a number!");
+                continue;
+            }
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal   => {
+                println!("You win!");
+                println!("It took {} tries to finish!", tries);
+                break;
+            }
+        }
+
+        println!("Current nr. of tries: {} \n", tries);
+    }
+}
